b88e1e98639d19aa661ac8d2ee3b57e0e0350007
Sara-SL <Sara.Styfberglundqvist.8518@student.uu.se>
2020-03-31 17:31:25 +0200

    removed fastqc.sh since created new scripts

--- Scripts/fastqc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-	for i in ../Data/sel4/wgs_data/*.fastq.gz
-        do
-          	echo "Running $i ..."
-                fastqc -t 1 -o ../1_quality_check/fastqc_wgs "$i"
-        done
