7b84fe500977910c75468767530ea781e3977b40
Sara-SL <Sara.Styfberglundqvist.8518@student.uu.se>
2020-04-01 18:19:33 +0200

    changed the path since I renamed the data

--- Scripts/fastqc_rna_seq_trimmed.sh
+++ Scripts/fastqc_rna_seq_trimmed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-	for i in ../Data/sel4/rna_seq_data/trimmed/*.fastq.gz
+	for i in ../Data/rna_seq_data/trimmed/*.fastq.gz
         do
           	echo "Running $i ..."
                 fastqc -t 1 -o ../1_quality_check/fastqc_rna_seq_trimmed "$i"
--- Scripts/fastqc_wgs.sh
+++ Scripts/fastqc_wgs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-	for i in ../Data/sel4/wgs_data/*.fastq.gz
+	for i in ../Data/wgs_data/*.fastq.gz
         do
           	echo "Running $i ..."
                 fastqc -t 1 -o ../1_quality_check/fastqc_wgs "$i"
