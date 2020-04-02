517d00b0bced012cd3eedf0f00830f4980bc3bc4
Sara-SL <Sara.Styfberglundqvist.8518@student.uu.se>
2020-04-02 18:20:10 +0200

    Changed to correct ouput-path

--- Scripts/fastqc_rna_seq_raw.sh
+++ Scripts/fastqc_rna_seq_raw.sh
@@ -3,5 +3,5 @@
 	for i in ../Data/rna_seq_data/raw/*.fastq.gz
         do
           	echo "Running $i ..."
-                fastqc -t 1 -o ../1_quality_check/fastqc_rna_seq_raw "$i"
+                fastqc -t 1 -o ../1_quality_check/fastqc_rna_seq/raw "$i"
         done
