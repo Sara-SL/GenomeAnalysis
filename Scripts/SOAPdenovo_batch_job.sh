60bb4a6d7a282d6f6d5647c4cc3b47333dbefa56
Sara-SL <Sara.Styfberglundqvist.8518@student.uu.se>
2020-04-15 16:29:37 +0200

    config file and batchjob for Soapdenovo

---
+++ Scripts/SOAPdenovo_batch_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 03:00:00
+#SBATCH --reservation=g2020008_15
+#SBATCH -J Genome_assembly_1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Sara.Styfberglundqvist.8518@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load soapdenovo
+
+# Your commands
+SOAPdenovo-127mer all -p 2 -s /home/sarasl/git/GenomeAnalysis/2_genome_assembly/SOAPdenovo.config -K 49 -R -o /home/sarasl/git/GenomeAnalysis/2_genome_assembly/SOAPdenovo_output_K49 1>ass.log 2>ass.err
