4cec01ab418abb546f39a4c4890248e020fe7323
Sara-SL <Sara.Styfberglundqvist.8518@student.uu.se>
2020-04-16 20:24:40 +0200

    added metadata, commands and SOAPdenovo run and result files

--- Scripts/SOAPdenovo_batch_job.sh
+++ Scripts/SOAPdenovo_batch_job.sh
@@ -3,7 +3,6 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 03:00:00
-#SBATCH --reservation=g2020008_15
 #SBATCH -J Genome_assembly_1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Sara.Styfberglundqvist.8518@student.uu.se
@@ -13,4 +12,4 @@ module load bioinfo-tools
 module load soapdenovo
 
 # Your commands
-SOAPdenovo-127mer all -p 2 -s /home/sarasl/git/GenomeAnalysis/2_genome_assembly/SOAPdenovo.config -K 49 -R -o /home/sarasl/git/GenomeAnalysis/2_genome_assembly/SOAPdenovo_output_K49 1>ass.log 2>ass.err
+SOAPdenovo-63mer all -p 2 -s /home/sarasl/git/GenomeAnalysis/2_genome_assembly/SOAPdenovo.config -K 49 -R -o /home/sarasl/git/GenomeAnalysis/2_genome_assembly/SOAPdenovo_output_K49 1>ass.log 2>ass.err
