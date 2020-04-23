559d30c176c5fc62352b632f5d605decc37ee152
Sara-SL <Sara.Styfberglundqvist.8518@student.uu.se>
2020-04-23 12:01:34 +0200

    fixed spelling error

--- Scripts/MUMmer_contig_100bp_batch_job.sh
+++ Scripts/MUMmer_contig_100bp_batch_job.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:30:00
+#SBATCH -t 04:00:00
 #SBATCH -J Assembly_evaluation_1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Sara.Styfberglundqvist.8518@student.uu.se
